eb7fa0b7e477042cb9de29ea1066cc519e5bb8c5
Sir-josh <docsmile01@gmail.com>
2025-10-16 04:47:06 +0100

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,11 @@
-export let cart = JSON.parse(localStorage.getItem("cart"));
-if (!cart) {
-  cart = [
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+   cart = JSON.parse(localStorage.getItem("cart"));
+    if (!cart) {
+     cart = [
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
       quantity: 2,
@@ -12,6 +17,8 @@ if (!cart) {
       deliveryOptionId: "2",
     },
   ];
+}
+
 }
 
 function saveToStorage() {
--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from '../script/utils/money.js';
+import { formatCurrency } from '../../script/utils/money.js';
 
 describe('Test-suite: formatCurrency', () => {
   it('Convert cents into dollars', () => {
